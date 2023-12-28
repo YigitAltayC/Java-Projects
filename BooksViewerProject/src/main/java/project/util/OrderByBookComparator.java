8fda338fda93eb6ba2669f886fbc24ce72e1beca
Yiğit Altay <yigitaltay.c@gmail.com>
2023-12-28 18:30:43 +0300

    Project Hiearachy and First Classes

--- BooksViewerProject/src/main/java/project/util/OrderByBookComparator.java
+++ BooksViewerProject/src/main/java/project/util/OrderByBookComparator.java
@@ -8,7 +8,7 @@ public class OrderByBookComparator implements Comparator<Book> {
 
     @Override
     public int compare(Book o1, Book o2) {
-        return o1.getBookName().compareTo(o2.getBookName());;
+        return o1.getBookName().compareTo(o2.getBookName());
     }
 
 }
