cb1d2c2aecbd0e96acfe75cf6590bfbe8948c892
Yiğit Altay <yigitaltay.c@gmail.com>
2024-02-12 23:59:47 +0300

    TodoApp Backend - JWT Configuration

--- Udemy/SpringBoot-FullStack Course/full-stack/01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
+++ Udemy/SpringBoot-FullStack Course/full-stack/01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/basic/BasicAuthenticationSecurityConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Configuration
+//@Configuration
 public class BasicAuthenticationSecurityConfiguration {
 
     @Bean
